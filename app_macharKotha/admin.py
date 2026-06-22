f4f2ce129493110bd41ad507411f8ae14d6646d0
Mostakim15 <mbmostakim5050@gmail.com>
2026-06-22 21:21:41 +0600

    admin register

--- app_macharKotha/admin.py
+++ app_macharKotha/admin.py
@@ -2,4 +2,25 @@ from django.contrib import admin
 from .models import Category, Tag, Post, Comment, Bookmark
 
 # Register your models here.
-admin.site.register()
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'created_at')
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'created_at')
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'created_at')
+    prepopulated_fields = {'slug': ('title',)}
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'post', 'created_at')
+
+@admin.register(Bookmark)
+class BookmarkAdmin(admin.ModelAdmin):
+    list_display = ('user', 'post', 'created_at')
